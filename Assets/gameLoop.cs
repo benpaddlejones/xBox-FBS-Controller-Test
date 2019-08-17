70ef5b39f8750baa563c2f7b3fbb38b12f892a51
Ben Jones <benpaddlejones@outlook.com>
2019-08-17 15:05:25 +1000

    Better xBox Controller Settings

---
+++ Assets/gameLoop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameLoop : MonoBehaviour
+{
+
+    public int bulletSpeed;
+    public Rigidbody bulletObject;
+    public Transform barrelEnd;
+
+    private void spawnBullet()
+    {
+        Rigidbody b;
+        b = Instantiate(bulletObject, new Vector3(barrelEnd.position.x, barrelEnd.position.y, barrelEnd.position.x), barrelEnd.rotation) as Rigidbody;
+        b.AddForce(b.transform.forward * bulletSpeed);
+    }
+
+    private void Update()
+    {
+    if (Input.GetAxis("Fire1") > 0 || Input.GetButtonDown("Fire1"))
+        {
+            spawnBullet();
+        }
+    }
+
+}
---
+++ Assets/terrain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrain : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Update()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+ 
+
+    }
+
+}
